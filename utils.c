74b24b96a88b59f41d804c52f3e0acd0db912d7f
Queenjul <julianainalegwu@gmail.com>
2023-04-26 03:57:09 -0700

    utils.c

--- utils.c
+++ utils.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * is_printable - Estimate if a char is printable
- * @c: Char to be estimated.
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -84,4 +84,3 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
-i
