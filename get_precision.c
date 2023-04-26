ba26025beec1fb8eb4dfefe9efc443a12e20e03c
Raphael <Raphealadu10@gmail.com>
2023-04-26 10:17:45 +0000

    get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,11 +1,10 @@
-
 #include "main.h"
 
 /**
- * get_precision - Compute the precision for printing
+ * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Lists of arguments to be printed.
- * @list: lists of arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
